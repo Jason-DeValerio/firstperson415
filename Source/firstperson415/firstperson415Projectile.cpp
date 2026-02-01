690454a59a300ce01a1a62224dec25e967f8bef8
Jason DeValerio <jason.devalerio@snhu.edu>
2026-02-01 14:55:07 -0500

    Added initial Niagara particle system

--- Source/firstperson415/firstperson415Projectile.cpp
+++ Source/firstperson415/firstperson415Projectile.cpp
@@ -6,6 +6,8 @@
 #include "Components/SphereComponent.h"
 #include "Components/DecalComponent.h"
 #include "Kismet/GameplayStatics.h"
+#include "NiagaraFunctionLibrary.h"
+#include "NiagaraComponent.h"
 
 Afirstperson415Projectile::Afirstperson415Projectile() 
 {
@@ -59,7 +61,18 @@ void Afirstperson415Projectile::OnHit(UPrimitiveComponent* HitComp, AActor* Othe
 
 	if (OtherActor != nullptr)
 	{
-		
+		if (colorP)
+		{
+			UNiagaraComponent* particleComp = UNiagaraFunctionLibrary::SpawnSystemAttached(colorP, HitComp, NAME_None, FVector(-20.f, 0.f, 0.f), FRotator(0.f), EAttachLocation::KeepRelativeOffset, true);
+
+			particleComp->SetNiagaraVariableLinearColor(FString("RandomColor"), randColor);
+
+			ballMesh->DestroyComponent();
+
+			CollisionComp->BodyInstance.SetCollisionProfileName("NoCollision");
+		}
+
+
 		float frameNum = UKismetMathLibrary::RandomFloatInRange(0.f, 3.f);
 		if (baseMat)
 		{
